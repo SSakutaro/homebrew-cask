5d70ca96bcab97e14e2a0026b3c0e69e660d5015
Sakutaro Shiina <companyemailetc@gmail.com>
2026-08-27 23:29:37 +0900

    virtual-ii: add user_agent

--- Casks/v/virtual-ii.rb
+++ Casks/v/virtual-ii.rb
@@ -2,7 +2,8 @@ cask "virtual-ii" do
   version "13.2"
   sha256 :no_check
 
-  url "https://virtualii.com/VirtualII.dmg"
+  url "https://virtualii.com/VirtualII.dmg",
+      user_agent: :browser
   name "Virtual ]["
   desc "Apple II Emulator"
   homepage "https://virtualii.com/"
