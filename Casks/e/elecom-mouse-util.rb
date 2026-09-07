c72f0d80e57da040d18d4377b0f7f3ab4e21e8cd
Sakutaro Shiina <companyemailetc@gmail.com>
2026-09-07 19:28:16 +0900

    elecom-mouse-util: livecheck test in CI Environment

--- Casks/e/elecom-mouse-util.rb
+++ Casks/e/elecom-mouse-util.rb
@@ -7,6 +7,7 @@ cask "elecom-mouse-util" do
   desc "Software to more effectively use an ELECOM mouse"
   homepage "https://www.elecom.co.jp/global/download-list/utility/mouse_assistant/mac/"
 
+  # livecheck test in CI Environment
   livecheck do
     url :homepage
     regex(/ELECOM[._-]MA[._-]Setup[._-]v?(\d+(?:\.\d+)+)\.zip/i)
